614555d49b6dab911b4cf8187bb1c49f4da44459
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2025-03-14 23:15:13 -0400

    updated codebuild to work with lambda type

--- labs/SQSVoteCollector/iac/pre_deploy.sh
+++ labs/SQSVoteCollector/iac/pre_deploy.sh
@@ -1,5 +1,7 @@
+DOWNLOAD=$1
 
-BUCKET=`aws ssm get-parameter --name "/Infra/State/Bucket/Name" | jq '.Parameter | .Value' | tr -d '"'`
-echo $BUCKET
-aws s3 cp s3://$BUCKET/labs/SQSVoteCollector/lambda/VoteCollector.zip modules/lambdas/VoteCollector.zip
-aws s3 cp s3://$BUCKET/labs/SQSVoteCollector/lambda/VoteGenerator.zip modules/lambdas/VoteGenerator.zip
+if [ -z "$CODEBUILD_BUILD_ID" ] || [ ! -f /tmp/VoteCollector.zip ]; then
+    BUCKET=`aws ssm get-parameter --name "/Infra/State/Bucket/Name" | jq '.Parameter | .Value' | tr -d '"'`
+    aws s3 cp s3://$BUCKET/labs/SQSVoteCollector/lambda/VoteCollector.zip /tmp/VoteCollector.zip
+    aws s3 cp s3://$BUCKET/labs/SQSVoteCollector/lambda/VoteGenerator.zip /tmp/VoteGenerator.zip
+fi
