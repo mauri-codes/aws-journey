042df488b045dc197f61a6870cc91c6684fb6757
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2025-05-07 23:23:03 -0400

    fixed local variables

--- labs/SQSVoteCollector/iac/modules/lambdas/locals.tf
+++ labs/SQSVoteCollector/iac/modules/lambdas/locals.tf
@@ -1,7 +1,7 @@
 locals {
   vote_collector_lambda_path = "VoteCollector"
   vote_generator_lambda_path = "VoteGenerator"
-  vote_collector_lambda_name = "${vote_collector_lambda_path}${var.suffix}"
-  vote_generator_lambda_name = "${vote_generator_lambda_path}${var.suffix}"
+  vote_collector_lambda_name = "${local.vote_collector_lambda_path}${var.suffix}"
+  vote_generator_lambda_name = "${local.vote_generator_lambda_path}${var.suffix}"
   generator_policy_name       = "LambdaSendSQS${var.suffix}"
 }
